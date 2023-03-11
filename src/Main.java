9af7f803ae17df7faf2c17bb54abc494db2d8263
youssefmahni <87309595+youssefmahni@users.noreply.github.com>
2023-03-12 00:59:38 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        //change the file path to its path in your computer in line 113 in Couter file
+        //change the file path to its path in your computer in line 113 in Counter file
         new Counter();
     }
 }
