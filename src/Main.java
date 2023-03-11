4327d5bee585115ab7340c06d65e6dc9077eaf88
youssefmahni <87309595+youssefmahni@users.noreply.github.com>
2023-03-12 00:57:15 +0100

    change the file path
    change the file path to its path in your computer in line 113 in Couter file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        //change the file path to its path in your computer in line 113 in Couter file
         new Counter();
     }
 }
